b8412fbcc9d6440ab9561caaa0d3d2128b70d9d7
Alexsandro Mello de Souza Lima <alexsandro.mello@gmail.com>
2025-10-21 19:16:31 -0300

    Add files via upload

---
+++ delivery-api/src/main/java/com/deliverytech/delivery/security/JwtAuthenticationFilter.java
@@ -0,0 +1,22 @@
+package com.deliverytech.delivery.security;
+
+import java.io.IOException;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
